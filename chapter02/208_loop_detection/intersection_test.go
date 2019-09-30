f7d88f36972eb4a019bbc0f6f4531f998d211c73
Tony Pujals <tpujals@gmail.com>
2019-09-30 08:30:18 -0700

    208 loop detection

---
+++ chapter02/208_loop_detection/intersection_test.go
@@ -0,0 +1,59 @@
+package loop_detection
+
+import (
+	"testing"
+
+	ds "github.com/subfuzion/coding-problems-in-go/datastructures"
+	"github.com/subfuzion/coding-problems-in-go/test"
+)
+
+// F is the type of function that implements the solution
+type F func(*ds.Node) *ds.Node
+
+// Test defines a test case
+type Test struct {
+	Input    *ds.Node
+	Expected *ds.Node
+}
+
+// Tests is a test suite
+var Tests = []Test{
+}
+
+func TestSolution(t *testing.T) {
+	for _, f := range Solutions {
+		t.Run(test.GetFileFuncName(f), func(t *testing.T) {
+			for _, test := range Tests {
+				actual := f(test.Input)
+				expected := test.Expected
+
+				if actual != expected {
+					t.Errorf("\nexpected % for %v", expected, test.Input)
+				}
+			}
+		})
+	}
+}
+
+// TODO - add your own solutions to the list below
+// Solutions maps a name (or brief description) to solution implementations
+var Solutions = []F{
+	detectLoop,
+	// detectLoop1,
+	// ...
+}
+
+// TODO - implement your own solution (and update the Solutions list, above)
+/*
+Loop Detection: Given a circular linked list, implement an algorithm that returns the node at the
+beginning of the loop.
+DEFINITION
+Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node,
+so as to make a loop in the linked list.
+EXAMPLE
+Input:  A -> B -> C -> D -> E -> C [the same C as earlier]
+Output: C
+ */
+func detectLoop1(node *ds.Node) *ds.Node {
+	return nil
+}
---
+++ chapter02/208_loop_detection/solution.go
@@ -0,0 +1,7 @@
+package loop_detection
+
+import ds "github.com/subfuzion/coding-problems-in-go/datastructures"
+
+func detectLoop(node *ds.Node) *ds.Node {
+	return nil
+}
